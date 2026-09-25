5b624e1d5bfab6a8e24c83ae0b145782e060b10e
Sai Sameera Nekkanti <sami.nekkanti@gmail.com>
2026-09-26 00:12:03 +0530

    Modify program to handle integer array input
    Replaced character array with integer array and updated input/output logic.

--- pointers/integerArray.c
+++ pointers/integerArray.c
@@ -1,29 +1,16 @@
 #include <stdio.h>
 int main(){
-    char a[100];
-    char *p;
+    int a[5];
+    int *p;
     int i;
-    int vowels=0;
     p=a;
-
-    printf("enter string: ");
-    scanf("%s", a);
-
-    for(int i=0; *(p+i)!='\0'; i++){
-        if(*(p+i)=='a'||*(p+i)=='e'||*(p+i)=='i'||*(p+i)=='o'||
-           *(p + i) == 'u' ||
-           *(p + i) == 'A' || *(p + i) == 'E' ||
-           *(p + i) == 'I' || *(p + i) == 'O' ||
-           *(p + i) == 'U'){
-                vowels++;
-                *(p+i) = '*';
-           }
+    printf("enter 5 integers\n");
+    for(int i=0; i<5; i++){
+        scanf("%d", &a[i]);
     }
-    printf("\nNumber of vowels = %d\n", vowels);
-
-    printf("Updated array: ");
-    for(i=0; *(p+i)!='\0';i++){
-        printf("%c", *(p+i));
+    printf("\nAddress and values:\n");
+    for(int i=0; i<5; i++){
+        printf("address= %p, value=%d\n", (void *)(p+i),*(p+i));
     }
     return 0;
 
